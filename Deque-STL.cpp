9bb783d5c6694de1c02f2ca0ab0a1afe8706ac8d
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-09-23 10:20:43 +0530

    Add files via upload

---
+++ Deque-STL.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <deque> 
+using namespace std;
+
+void printKMax(int arr[], int n, int k){
+    deque<int> q;
+    for(int i=0;i<n;i++)
+    {
+        for(;!q.empty() && arr[i]>q.back();)
+        {
+            q.pop_back();
+        }
+              
+        q.push_back(arr[i]);  
+        if(i>=k && q.front()==arr[i-k])
+        {
+            q.pop_front();
+        }
+              
+        if(i >= k-1)
+        {
+            
+            if(i<n-1)
+            {
+             cout<<q.front()<<" ";   
+            }
+            else 
+            {
+                cout<<q.front() << " "<<endl;
+            }
+        }
+            
+    }
+	//Write your code here.
+}
+
+int main(){
+  
+	int t;
+	cin >> t;
+	while(t>0) {
+		int n,k;
+    	cin >> n >> k;
+    	int i;
+    	int arr[n];
+    	for(i=0;i<n;i++)
+      		cin >> arr[i];
+    	printKMax(arr, n, k);
+    	t--;
+  	}
+  	return 0;
+}
